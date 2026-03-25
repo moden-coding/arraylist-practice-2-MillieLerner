dfdcc8dab28ee43967a9e2a19497bf821f0c52bb
MillieLerner <28mlerner@gannacademy.org>
2026-03-25 13:24:25 -0400

    Done

--- src/Feed.java
+++ src/Feed.java
@@ -7,11 +7,30 @@ public class Feed {
 
         // Step 2: Add 7 post descriptions using .add()
         // Example: feed.add("Maya posted a photo");
+        int count=1;
+
+        feed.add("Maia liked a photo");
+        feed.add("Penny liked a photo");
+        feed.add("Lyra posted a photo");
+        feed.add("Aeden posted a photo");
+        feed.add("Rose liked a video");
+        feed.add("Penny liked a video");
+        feed.add("Lyra posted a photo");
+         feed.add("Tomer posted a photo");
 
 
         // Step 3: Print only the FIRST 3 posts (the top of the feed)
         // Use a for loop — think carefully about when your loop should stop
-
+        int i=0;
+        int p=feed.size()-5;
+        while(i<=feed.size()-4){
+            System.out.println(count+". "+feed.get(i));
+            i++;
+            count++;
+        
+        }
+            System.out.println("There are "+p+" posts waiting");
+           
 
         // Step 4: Print how many posts are still waiting below the top 3
         
